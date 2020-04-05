d0aebeb1467f7cebf81188c17602060636d321f6
Cindy Okino <cindy.okino@gmail.com>
2020-04-05 14:02:47 -0400

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,6 +25,12 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition(); // converte de posicao de matriz para posicao normal
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition(); // transformar em posicao no formato matriz
 		Position target = targetPosition.toPosition(); // transformar em posicao no formato matriz
